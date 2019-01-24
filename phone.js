df618b5a5b97a033abd505e8551152c5f48d6c79
Dillon <dillonroxer@gmail.com>
2019-01-23 17:26:11 -0700

    Firstpush

---
+++ phone.js
@@ -0,0 +1,4 @@
+$(document).ready(function() { // do this when the document is loaded
+	$("#element").show(); // show the element with ID "element"
+	$("#otherElement").hide(); // hide the element with ID "otherElement"
+});
